27e65c27823078ff0e6ba57065320da84a023371
MoahmedAzar <azar.maasconsult@gmail.com>
2025-07-17 22:15:10 +0300

    Create hooks.py

---
+++ saudi_riyal_symbol/hooks.py
@@ -0,0 +1,8 @@
+app_name = "saudi_riyal_symbol"
+app_title = "Saudi Riyal Symbol"
+app_publisher = "Mohamed Azarudeen"
+app_description = "Injects custom Saudi Riyal symbol"
+app_email = "azar.maasconsult@gmail.com"
+app_license = "MIT"
+
+app_include_css = ["/assets/saudi_riyal_symbol/symbol.css"]
